6ab81ed58a84a59ea72b20b7b547f667ee289a35
Nikolai Mushegian <nikolai.mushegian@gmail.com>
2015-11-29 20:18:04 -0500

    dist snapshot

--- maker.js
+++ maker.js
@@ -1,21 +1,28 @@
-// Need to apply `brfs` transform to bundle this for browser
 var fs = require("fs");
 var Web3 = require("web3");
 var web3 = new Web3();
 var dappsys = require("./dapple_packages/dappsys/build/js_module.js");
 var objects = JSON.parse(fs.readFileSync("./env/objects.json"));
 
-module.exports = (function() {
+var Maker = function() {
+    // Need to apply `brfs` transform to bundle this for browser
+
     this.web3 = web3;
     this.objects = objects;
     this.header = dappsys; // TODO merge other dependencies
     for( var key of Object.keys(objects) ) {
         var classname = this.objects[key].typename;
         var abi = JSON.parse(this.header[classname]["interface"]);
-        var contract = web3.eth.contract(abi);
+        var contract = this.web3.eth.contract(abi);
         var address = this.objects[key].address;
         var instance = contract.at(address);
         this.objects[key].instance = instance
-        console.log("Instantiated object " + key + " with classname " + classname);
     }
-})();
+
+    this.getBalance = function(symbol, addr) {
+        return this.objects[symbol].instance.get_balance(addr);
+    }
+};
+var maker = new Maker();
+module.exports.maker = maker;
+window.maker = maker;
